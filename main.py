5c52d2b138c99b40c83a74f63f4e018cad51d2be
JurajSefcik <juraj.sefcik@nordlicht-it.com>
2023-05-03 11:09:14 +0200

    third commit.. if not working im commiting sucide.....

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": "Application ran successfully - FastAPI"}
+    return {"data": "Application ran successfully - FastAPI V2!"}
 
 
 if __name__ == "__main__":
