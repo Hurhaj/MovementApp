8f4e840d64d79d37dc14c8067c3d32ef842f3f71
JurajSefcik <juraj.sefcik@nordlicht-it.com>
2023-05-03 14:35:19 +0200

    this is the working version and adding another API... hopefully it passes

--- main.py
+++ main.py
@@ -11,16 +11,23 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": "Application ran successfully - FastAPI V4!"}
+    return {"data": "Application ran successfully - FastAPI V5! checkout apis: /hello /hens /another"}
 
 
 @app.get("/hello")
 def hello():
     return {"data": "Hello hens!"}
 
+
 @app.get("/hens")
 def hens():
     return {"data": "Hey hens!"}
+
+
+@app.get("/another")
+def another():
+    return {"datas": "Another deploy"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
