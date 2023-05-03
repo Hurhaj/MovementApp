a70762900aff3d6cfefc2e815e8aa82d13ac9270
JurajSefcik <juraj.sefcik@nordlicht-it.com>
2023-05-03 13:51:21 +0200

    deploying new API /hello -> response {"data": "Hello hens!"}

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def index():
     return {"data": "Application ran successfully - FastAPI V4!"}
 
 
+@app.get("/hello")
+def hello():
+    return {"data": "Hello hens!"}
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
