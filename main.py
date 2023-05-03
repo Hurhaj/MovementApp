dff91271713a7414a4ce72f825e4a200c94a0288
Štefan Kissi <stefan.kissi@student.tuke.sk>
2023-05-03 21:38:42 +0200

    porteeee

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ async def return_elevation(locations : List[location]):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
