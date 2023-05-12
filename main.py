b7989f9b8d4e3997949f12e0fa2866a301b6af44
JurajSefcik <juraj.sefcik@nordlicht-it.com>
2023-05-12 12:14:18 +0200

    google-auth fixed, installed in Pipfile.lock

--- main.py
+++ main.py
@@ -11,23 +11,27 @@ from datetime import datetime, timezone
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
+
 class Location(BaseModel):
     latitude: float
     longitude: float
 
+
 class Delete(BaseModel):
     token: str
     ID: str
+
 
 class Authenticated(BaseModel):
     email: str
     error: bool
 
 
-class Routepoints(BaseModel):
+class RoutePoints(BaseModel):
     latitude: float
     longitude: float
     altitude: float
+
 
 class Activity(BaseModel):
     ID: str
@@ -37,18 +41,23 @@ class Activity(BaseModel):
     time: str
     max_speed: int
     average_speed: int
-    data: List[Routepoints]
+    data: List[RoutePoints]
 
-class Synchronizationrequest(BaseModel):
+
+class SynchronizationRequest(BaseModel):
     token: str
     IDs: List[str]
 
-class Synchronizationanswer(BaseModel):
+
+class SynchronizationAnswer(BaseModel):
     activities: List[Activity]
     IDs: List[str]
 
-class Newactitivityreceive(BaseModel):
+
+class NewActitivityReceive(BaseModel):
     activities: List[Activity]
+
+
 # load environment variables
 port = os.environ["PORT"]
 connection_string = "mongodb+srv://user:user@cluster0.hbniblw.mongodb.net/?retryWrites=true&w=majority"
@@ -63,27 +72,34 @@ app = FastAPI()
 def index():
     return {"data": "Application ran successfully - FastAPI V5! checkout apis: /hello /hens /another"}
 
+
 @app.post("/mongo")
-async def put(user:str):
+async def put(user: str):
     doc = {"user": user}
     try:
         news = db["users"].insert_one(doc)
         return "done"
     except Exception as e:
-        return e+"error"
+        return e + "error"
+
 
 @app.post("/erase")
-async def erase(deleteactivity : Delete):
+async def erase(deleteactivity: Delete):
     return authentificate(deleteactivity.token)
-def return_elevation(locations : List[Location]):
+
+
+def return_elevation(locations: List[Location]):
     elevation_data = srtm.get_data()
     elevations = []
     for lo in locations:
-        elevations.append(elevation_data.get_elevation(lo.latitude,lo.longitude))
+        elevations.append(elevation_data.get_elevation(lo.latitude, lo.longitude))
     return elevations
-def authentificate(token:str):
+
+
+def authentificate(token: str):
     try:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), "168397874560-5uso2lk8p5pa43h3sb3eg9futfisese0.apps.googleusercontent.com")
+        idinfo = id_token.verify_oauth2_token(token, requests.Request(),
+                                              "168397874560-5uso2lk8p5pa43h3sb3eg9futfisese0.apps.googleusercontent.com")
         expiration_time = datetime.fromtimestamp(idinfo['exp'], timezone.utc)
         if datetime.now(timezone.utc) >= expiration_time:
             return Authenticated(email="token expired", error=True)
@@ -94,5 +110,7 @@ def authentificate(token:str):
         return Authenticated(email="Decode_error", error=True)
     except jwt.exceptions.InvalidTokenError:
         return Authenticated(email="Invalid_token_error", error=True)
+
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
